41b3f91c5cea53e2615974e80050306491dafff3
Assanali18 <ustaevasanali@gmail.com>
2024-09-19 12:37:14 +0500

    add 1,2 ex

---
+++ lab2/GORM/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID   uint `gorm:"primary_key"`
+	Name string
+	Age  int
+}
+
+func main() {
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Ошибка загрузки файла .env: %v", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+	fmt.Println("Подключение к базе данных успешно")
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		log.Fatalf("Ошибка миграции: %v", err)
+	}
+	fmt.Println("Миграция успешно завершена")
+
+	insertData(db)
+
+	queryData(db)
+}
+
+func insertData(db *gorm.DB) {
+	users := []User{
+		{Name: "Asan", Age: 19},
+		{Name: "Alisher", Age: 20},
+		{Name: "Aibek", Age: 21},
+	}
+	result := db.Create(&users)
+	if result.Error != nil {
+		fmt.Println("Ошибка вставки данных: ", result.Error)
+	} else {
+		fmt.Println("Данные успешно вставлены")
+	}
+}
+
+func queryData(db *gorm.DB) {
+	var users []User
+
+	result := db.Find(&users)
+	if result.Error != nil {
+		fmt.Println("Ошибка запроса данных: ", result.Error)
+	} else {
+		fmt.Printf("Found %d users\n", result.RowsAffected)
+		for _, user := range users {
+			fmt.Printf("ID: %d, Name: %s, Age: %d\n", user.ID, user.Name, user.Age)
+		}
+	}
+}
---
+++ lab2/directConnect/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "Asan18102004"
+	dbname   = "go_lab2"
+)
+
+func connectDB() (*sql.DB, error) {
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Ошибка загрузки файла .env: %v", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Successfully connected!")
+	return db, nil
+}
+
+func createTable(db *sql.DB) {
+	query := `
+CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50),
+        age INT
+    );
+`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatalf("Unable to create table", err)
+	}
+	fmt.Println("Table created successfully")
+}
+
+func insertUser(db *sql.DB, name string, age int) {
+	query := `
+INSERT INTO users (name, age)
+    VALUES ($1, $2)
+`
+	_, err := db.Exec(query, name, age)
+	if err != nil {
+		log.Fatalf("Unable to insert user", err)
+	}
+	fmt.Printf("Inserted user: %s, Age: %d\n", name, age)
+}
+
+func queryUsers(db *sql.DB) {
+	query := `
+SELECT id, name, age FROM users
+`
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatalf("Unable to query users", err)
+	}
+	fmt.Println("Users:")
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		var age int
+		err = rows.Scan(&id, &name, &age)
+		if err != nil {
+			log.Fatalf("Unable to scan row", err)
+		}
+		fmt.Printf("ID: %d, Name: %s, Age: %d\n", id, name, age)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatalf("Error in rows", err)
+	}
+}
+
+func main() {
+	db, error := connectDB()
+	if error != nil {
+		log.Fatalf("Unable to connect to database", error)
+	}
+	defer db.Close()
+
+	createTable(db)
+
+	insertUser(db, "Asan", 19)
+	insertUser(db, "Alisher", 20)
+	insertUser(db, "Aibek", 21)
+
+	queryUsers(db)
+
+}
